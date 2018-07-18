06327ca7d65cba6b8af5c9b5e20168f14f61ffba
Lance Fallon <fallon.lance@gmail.com>
2018-07-18 19:54:21 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, FormGroupDirective, Validators} from '@angular/forms';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  superheroForm: FormGroup;
+
+  constructor(private matSnackBar: MatSnackBar){}
+
+  onSubmit(formDirective: FormGroupDirective){
+    this.matSnackBar.open(`${this.superheroForm.value['alias']} has been added!`, '', {duration: 4000});
+    formDirective.resetForm();
+    this.superheroForm.reset();
+  }
+
+  ngOnInit(): void {
+    this.superheroForm = this.generateForm();
+  }
+
+  addSuit(): void{
+    (<FormArray>this.superheroForm.get('suits')).push(this.generateSuit())
+  }
+
+  removeSuit(index: number): void{
+    (<FormArray>this.superheroForm.get('suits')).removeAt(index);
+  }
+
+  generateForm(): FormGroup{
+    return new FormGroup({
+      'alias' : new FormControl(null, [Validators.required]),
+      'fname' : new FormControl(null, [Validators.required]),
+      'lname' : new FormControl(null, [Validators.required]),
+      'powerLevel': new FormControl(1000, [Validators.required]),
+      'suits' : new FormArray([ this.generateSuit() ])
+    })
+  }
+
+  generateSuit(): FormGroup{
+    return new FormGroup({
+      'description' : new FormControl(null, Validators.required),
+      'gloves' : new FormControl(true),
+      'boots' : new FormControl(true),
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule, MatCheckboxModule, MatChipsModule, MatFormFieldModule, MatInputModule, MatSnackBarModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatCardModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
